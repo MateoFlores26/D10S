733c95148f1e47be7c46b1a4f5f90911aa6ce337
MateoFlores26 <floresmateojoaquin04@gmail.com>
2023-11-15 16:44:52 -0300

    Agregando comentarios al codigo

--- logic.js
+++ logic.js
@@ -1,43 +1,41 @@
 // DOM Elements
-const hourEl = document.querySelector('.hour');
-const minuteEl = document.querySelector('.minute');
-const valueEl = document.querySelector('.value');
+const valueEl = document.querySelector('.value'); // Selección del elemento que muestra el valor actual en la calculadora
 
-const acEl = document.querySelector('.ac');
-const squareEl = document.querySelector('.square'); // New element for square operation
-const squareRootEl = document.querySelector('.square-root'); // New element for square root operation
+const acEl = document.querySelector('.ac'); // Selección del elemento para borrar la operación actual en la calculadora
+const squareEl = document.querySelector('.square'); // Nuevo elemento para la operación de elevar al cuadrado
+const squareRootEl = document.querySelector('.square-root'); // Nuevo elemento para la operación de raíz cuadrada
 
-const additionEl = document.querySelector('.addition');
-const subtractionEl = document.querySelector('.subtraction');
-const multiplicationEl = document.querySelector('.multiplication');
-const divisionEl = document.querySelector('.division');
-const equalEl = document.querySelector('.equal');
+const additionEl = document.querySelector('.addition'); // Selección del elemento para la operación de suma
+const subtractionEl = document.querySelector('.subtraction'); // Selección del elemento para la operación de resta
+const multiplicationEl = document.querySelector('.multiplication'); // Selección del elemento para la operación de multiplicación
+const divisionEl = document.querySelector('.division'); // Selección del elemento para la operación de división
+const equalEl = document.querySelector('.equal'); // Selección del elemento para obtener el resultado de la operación
 
-const decimalEl = document.querySelector('.decimal');
-const number0El = document.querySelector('.number-0');
-const number1El = document.querySelector('.number-1');
-const number2El = document.querySelector('.number-2');
-const number3El = document.querySelector('.number-3');
-const number4El = document.querySelector('.number-4');
-const number5El = document.querySelector('.number-5');
-const number6El = document.querySelector('.number-6');
-const number7El = document.querySelector('.number-7');
-const number8El = document.querySelector('.number-8');
-const number9El = document.querySelector('.number-9');
+const decimalEl = document.querySelector('.decimal'); // Selección del elemento para añadir un punto decimal
+const number0El = document.querySelector('.number-0'); // Selección del elemento para el número 0
+const number1El = document.querySelector('.number-1'); // Selección del elemento para el número 1
+const number2El = document.querySelector('.number-2'); // Selección del elemento para el número 2
+const number3El = document.querySelector('.number-3'); // Selección del elemento para el número 3
+const number4El = document.querySelector('.number-4'); // Selección del elemento para el número 4
+const number5El = document.querySelector('.number-5'); // Selección del elemento para el número 5
+const number6El = document.querySelector('.number-6'); // Selección del elemento para el número 6
+const number7El = document.querySelector('.number-7'); // Selección del elemento para el número 7
+const number8El = document.querySelector('.number-8'); // Selección del elemento para el número 8
+const number9El = document.querySelector('.number-9'); // Selección del elemento para el número 9
 const numberElArray = [
   number0El, number1El, number2El, number3El, number4El,
   number5El, number6El, number7El, number8El, number9El
-];
+]; // Array de elementos para los números
 
 // variables
-let valueStrInMemory = null;
-let operatorInMemory = null;
+let valueStrInMemory = null; // Variable para almacenar el valor en memoria
+let operatorInMemory = null; // Variable para almacenar el operador en memoria
 
 // Functions
-const getValueAsStr = () => valueEl.textContent.split(',').join('');
+const getValueAsStr = () => valueEl.textContent.split(',').join(''); // Función para obtener el valor como cadena
 
 const getValueAsNum = () => {
-  return parseFloat(getValueAsStr());
+  return parseFloat(getValueAsStr()); // Función para obtener el valor como número
 };
 
 const setStrAsValue = (valueStr) => {
@@ -53,7 +51,7 @@ const setStrAsValue = (valueStr) => {
   } else {
     valueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
   }
-};
+}; // Función para establecer una cadena como valor
 
 const handleNumberClick = (numStr) => {
   const currentValueStr = getValueAsStr();
@@ -62,7 +60,7 @@ const handleNumberClick = (numStr) => {
   } else {
     setStrAsValue(currentValueStr + numStr);
   }
-};
+}; // Función para manejar el clic en un número
 
 const getResultOfOperationAsStr = () => {
   const currentValueNum = getValueAsNum();
@@ -79,7 +77,7 @@ const getResultOfOperationAsStr = () => {
   }
 
   return newValueNum.toString();
-};
+}; // Función para obtener el resultado de la operación como cadena
 
 const handleOperatorClick = (operation) => {
   const currentValueStr = getValueAsStr();
@@ -93,22 +91,22 @@ const handleOperatorClick = (operation) => {
   valueStrInMemory = getResultOfOperationAsStr();
   operatorInMemory = operation;
   setStrAsValue('0');
-};
+}; // Función para manejar el clic en un operador matemático
 
 // Add Event Listeners to functions
 acEl.addEventListener('click', () => {
   setStrAsValue('0');
   valueStrInMemory = null;
   operatorInMemory = null;
-});
-squareEl.addEventListener('click', () => { // Event listener for the square operation
+}); // Evento al hacer clic en AC (limpiar la calculadora)
+squareEl.addEventListener('click', () => { // Evento al hacer clic en el botón para elevar al cuadrado
   const currentValueNum = getValueAsNum();
   const newValueNum = currentValueNum * currentValueNum;
   setStrAsValue(newValueNum.toString());
   valueStrInMemory = null;
   operatorInMemory = null;
 });
-squareRootEl.addEventListener('click', () => { // Event listener for the square root operation
+squareRootEl.addEventListener('click', () => { // Evento al hacer clic en el botón para raíz cuadrada
   const currentValueNum = getValueAsNum();
   const newValueNum = Math.sqrt(currentValueNum);
   setStrAsValue(newValueNum.toString());
@@ -119,23 +117,23 @@ squareRootEl.addEventListener('click', () => { // Event listener for the square 
 // Add event listeners to operators
 additionEl.addEventListener('click', () => {
   handleOperatorClick('addition');
-});
+}); // Evento al hacer clic en el botón de suma
 subtractionEl.addEventListener('click', () => {
   handleOperatorClick('subtraction');
-});
+}); // Evento al hacer clic en el botón de resta
 multiplicationEl.addEventListener('click', () => {
   handleOperatorClick('multiplication');
-});
+}); // Evento al hacer clic en el botón de multiplicación
 divisionEl.addEventListener('click', () => {
   handleOperatorClick('division');
-});
+}); // Evento al hacer clic en el botón de división
 equalEl.addEventListener('click', () => {
   if (valueStrInMemory) {
     setStrAsValue(getResultOfOperationAsStr());
     valueStrInMemory = null;
     operatorInMemory = null;
   }
-});
+}); // Evento al hacer clic en el botón de igual (=)
 
 // Add Event Listeners to numbers and decimal
 for (let i = 0; i < numberElArray.length; i++) {
@@ -143,26 +141,14 @@ for (let i = 0; i < numberElArray.length; i++) {
   numberEl.addEventListener('click', () => {
     handleNumberClick(i.toString());
   });
-}
+} // Eventos al hacer clic en los botones de los números y el decimal
+
 decimalEl.addEventListener('click', () => {
   const currentValueStr = getValueAsStr();
   if (!currentValueStr.includes('.')) {
     setStrAsValue(currentValueStr + '.');
   }
-});
+}); // Evento al hacer clic en el botón decimal (punto)
 
-// Set up the time
-const updateTime = () => {
-  const currentTime = new Date();
 
-  let currentHour = currentTime.getHours();
-  const currentMinute = currentTime.getMinutes();
 
-  if (currentHour > 12) {
-    currentHour -= 12;
-  }
-  hourEl.textContent = currentHour.toString();
-  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
-};
-setInterval(updateTime, 1000);
-updateTime();
